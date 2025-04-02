1da84bb84c93961fcaddb83ad8be0aca3e7ebeba
Kaua reis <kauadragon036@gmail.com>
2025-04-02 15:50:01 -0300

    finalização do projeto "Criar conta em banco".

--- Simulando Uma Conta Bancária Através Do Terminal/Conta Banco/src/ContaTerminal.java
+++ Simulando Uma Conta Bancária Através Do Terminal/Conta Banco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
 public class ContaTerminal {
      public static void main(String[] args){
-        System.out.println("Hello world");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Olá, sou o assistente virtual do banco!\n Por favor, digite o número da conta: ");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Agora, digite o número da agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Me diga qual o seu primeiro nome: ");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Agora o seu sobrenome: ");
+        String sobrenomeCliente = scanner.next();
+
+        System.out.println("Qual o saldo presente em sua conta? R$");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + " " + sobrenomeCliente + ", obrigado por criar uma conta em nosso banco,\n sua agência é " + agencia +", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque!");
      }
 }
